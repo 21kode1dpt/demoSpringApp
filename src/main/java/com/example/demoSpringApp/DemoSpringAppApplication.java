960407c9c6045805ad5b81e5152f8d69652e687f
denislavkszv <82kode1bwi@hft-stuttgart.de>
2022-12-11 16:41:19 +0100

    Implemented extended REST app

--- src/main/java/com/example/demoSpringApp/DemoSpringAppApplication.java
+++ src/main/java/com/example/demoSpringApp/DemoSpringAppApplication.java
@@ -1,9 +1,11 @@
 package com.example.demoSpringApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DemoSpringAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demoSpringApp/rest/Popularity.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoSpringApp.rest;
-
-public enum Popularity {
-    HIGH,
-    MEDIUM,
-    LOW
-}
